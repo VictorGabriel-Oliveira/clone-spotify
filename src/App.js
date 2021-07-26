03db067ed6d2d647cb51d66db3bebb9f56b2816a
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2021-07-26 11:05:59 -0300

    geting and seting token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import {ajax}  from 'jquery'
+import { Login } from './components/Login/Login';
+import { Player } from './components/Player/Player';
+import { getTokenFromResponse } from './Spotify';
 
 function App() {
-  (
-    <div>
-      hello world
-    </div>
-  )
+    const [token, setToken] = useState('')
+    useEffect(()=>{
+
+        const hash = getTokenFromResponse()
+        window.location.hash = ''
+        const _token = hash.access_token
+
+        if(_token){
+            setToken(_token)
+        }
+
+    },[])
+
+    return (
+        <div className="conteiner">
+            {
+                token ?  <Player/> : <Login/>
+            }
+        </div>
+    )
 }
 
 export default App;
