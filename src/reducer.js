3228fd43829e4265712dc578e15a3c367388b9ff
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2021-08-01 19:43:14 -0300

    add set token reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,20 +1,37 @@
+import { type } from "jquery"
+
 export const initialState = {
     user:null,
     playlist:[],
     playling:false,
-    item:null
+    item:null,
+    token:null
 }
 export const reducer = (state, aciton)=>{
     console.log(aciton)
+    
     switch(aciton.type){
         case "SET_USER" :
+            
             return{
                 ...state,
                 user: aciton.user
+               
             }
+
+        case 'SET_TOKEN':{
+            return {
+                ...state,
+                token:aciton.token
+            }
+        }
+
         default:
             return {
                 state
             }
     }
+
+    
+    
 }
