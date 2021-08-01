79479507a553b8c26dd2e1dd9f6c54659e2a6cfe
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2021-08-01 19:44:05 -0300

    add set token

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import { Login } from './components/Login/Login';
 import { Player } from './components/Player/Player';
 import { getTokenFromResponse } from './Spotify';
 
+import SpotifyWebApi from 'spotify-web-api-js'
+import { useDataLayerValue } from './context/DataLayer';
+
+const spotify = new SpotifyWebApi()
+
 function App() {
     const [token, setToken] = useState('')
+    const [{  user },dispatch] = useDataLayerValue()
+
+   
     useEffect(()=>{
 
         const hash = getTokenFromResponse()
@@ -13,15 +21,32 @@ function App() {
         const _token = hash.access_token
 
         if(_token){
+            
             setToken(_token)
+
+            dispatch({
+                type:'SET_TOKEN',
+                token:_token
+            })
+            spotify.setAccessToken(_token)
+            spotify.getMe().then(user =>{
+                
+                dispatch ({ 
+                    type: 'SET_USER',
+                    user
+
+                })
+            })
         }
+
+         
 
     },[])
 
     return (
         <div className="conteiner">
             {
-                token ?  <Player/> : <Login/>
+                token ?  <Player spotify={spotify}/> : <Login/>
             }
         </div>
     )
