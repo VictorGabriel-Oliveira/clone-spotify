89d4b77103780a7f9c4ffe7c58c6ebebee880818
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2021-08-03 22:09:33 -0300

    create footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './footer.css'
+
+export default function Footer (){
+    return (
+        <div className="footer" >
+            <h1>
+                I am Footer
+            </h1>
+        </div>
+    )
+}
