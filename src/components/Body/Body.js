fa50e1ad293065dc62956bcddd3c8366e29c1917
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2021-08-03 22:08:18 -0300

    creating Body component

---
+++ src/components/Body/Body.js
@@ -0,0 +1,11 @@
+import './body.css'
+
+export default function Body(){
+    return (
+        <div className="body">
+            <h1>
+                BODY
+            </h1>
+        </div>
+    )
+}
