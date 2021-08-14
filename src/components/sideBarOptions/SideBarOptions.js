e819326c36d7b484fba9f6f61bebbc959118c938
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2021-08-14 17:20:14 -0300

    creating side bar options component

---
+++ src/components/sideBarOptions/SideBarOptions.js
@@ -0,0 +1,22 @@
+import './sideBarOptions.css'
+export default function SideBarOptions({text , Icon}){
+    if (Icon){
+        return(
+            <div className="sideBarOpitions">
+                <p className="sideBar-links">
+                    <Icon/> {text} 
+                </p>
+            </div>
+        )
+    }
+    else{
+        return(
+            <div className="sideBarOpitions">
+                <p className="sideBar-links">
+                    {text} 
+                </p>
+            </div>
+            )
+    }
+   
+}
