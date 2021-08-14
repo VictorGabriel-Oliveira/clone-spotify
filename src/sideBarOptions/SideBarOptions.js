af0adabda0fb7a28f85e830a259aa3462014bf33
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2021-08-14 19:39:40 -0300

    deleted files

--- src/sideBarOptions/SideBarOptions.js
+++
@@ -1,9 +0,0 @@
-import './sideBarOpitions.css'
-export default function SideBarOpitions({text , Icon}){
-    console.log(Icon)
-    return(
-        <div className="sideBarOpitions">
-            <p className="sideBar-links"><Icon/> {text}</p>
-        </div>
-    )
-}
