690bffa37ee015bae01baccb277cc41d1a213249
victor gabriel <deoliveiraabreuvictorgabriel@gmail.com>
2021-08-22 08:11:29 -0300

    finishing song row components

--- src/components/SongRow/index.js
+++ src/components/SongRow/index.js
@@ -1,10 +1,10 @@
 import './index.css'
 
-export default function SongRow({track, playSong}){
+export default function SongRow({key , track, playSong}){
     return(
-        <div className="songRow" onClick={()=>{playSong(track.id)}}>
+        <div  key ={key} className="songRow" onClick={()=>{playSong(track.id)}}>
             <img className="songRow__album" src={track.album.images[0].url} />
-            <div cassName="songRow__info">
+            <div className="songRow__info">
                 <h1>{track.name}</h1>
                 <p > 
                     { track.artists.map((artist) => artist.name).join(", ")} -{" "}
